f7d68d46c98592cd1c7417c9fc0811e67643d239
RecursiveDeveloper <johanjesus133@hotmail.com>
2026-05-05 22:49:14 -0500

    IAC templates were deleted. server folder renaming test

--- Iac/Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket          = "backend-terraform-simple"
-        key             = "prod-state"
-        region          = "us-east-1"
-#        encrypt         = true
-#        kms_key_id      = "alias/Simple-key"
-    }
-}
